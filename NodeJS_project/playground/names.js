6b90886703e186059e708312002bb2b4e64bb772
Tomasz Paruzel <tom.krzy.par@gmail.com>
2023-07-31 10:02:33 +0200

    geo location and easy error handling

--- NodeJS_project/playground/names.js
+++
@@ -1,9 +0,0 @@
-const listNames = (names) => {
-    names.forEach((name) => {
-        console.log(name)        
-    })
-}
-
-const myNames = ["Tommy", "Paul"]
-
-listNames(myNames)
--- NodeJS_project/weather-app/app.js
+++ NodeJS_project/weather-app/app.js
@@ -1,12 +1,37 @@
 const request = require('postman-request')
 
-const url = "http://api.weatherstack.com/current?access_key=3b54beba076d64de34fc95e1e245a1df&query=54.632778,18.510278&units=m"
+// const url = "http://api.weatherstack.com/current?access_key=3b54beba076d64de34fc95e1e245a1df&query=54.632778,18.510278&units=m"
 
-request({url: url, json: true}, (error, response) => {
-    const temperature = (response.body.current.temperature)
-    const cloudcover = (response.body.current.cloudcover)
-    const weather_desc = (response.body.current.weather_descriptions[0])
+// request({url: url, json: true}, (error, response) => {
+    
+//     if(error) {
+//         console.log("Unable to connect to weather service!!")
+//     } else if (response.body.error) {
+//         console.log("Unable to find location")
+//     } else {
+//         const temperature  = (response.body.current.temperature)
+//         const cloudcover   = (response.body.current.cloudcover)
+//         const weather_desc = (response.body.current.weather_descriptions[0])
+        
+//         console.log("It is " + weather_desc + " and it is " + temperature + " degrees")
+//         console.log("The cloud cover is " + cloudcover)
+//     }
 
-    console.log("It is " + weather_desc + " and it is " + temperature + " degrees")
-    console.log("The cloud cover is " + cloudcover)
+// })
+
+const url_geocoding= "https://api.mapbox.com/geocoding/v5/mapbox.places/Rewa%20Poland.json?access_token=pk.eyJ1IjoidG9ta3J6eXBhciIsImEiOiJjbDhoOHpuMDcwNnh3M25sYzVzb3h0dzBhIn0.-JqTKCT9O509P56ldQgqCQ&limit=1"
+
+request({url: url_geocoding, json: true}, (error, response) => {
+
+    if(error) {
+        console.log("Unable to connect to location service!!")
+    } else if (response.body.features.length === 0) {
+        console.log("Unable to find location")
+    } else {
+        const lon = (response.body.features[0].center[0])
+        const lat = (response.body.features[0].center[1])
+
+        console.log("The longitude is: "+lon)
+        console.log("The latitude is: "+lat)
+    }
 })
