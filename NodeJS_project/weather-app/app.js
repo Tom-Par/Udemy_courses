b56d0a4aac1f6e7fd732eb4e2d49afb6b2583787
Tomasz Paruzel <tom.krzy.par@gmail.com>
2023-08-01 22:01:14 +0200

    Callback chaining

--- NodeJS_project/weather-app/app.js
+++ NodeJS_project/weather-app/app.js
@@ -1,12 +1,24 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-forecast(54.6327, 18.5106405, (error, data) => {
-    console.log('Error:', error)
-    console.log('Data:', data)
-})
+const address = process.argv[2]
 
-geocode("Rewa Poland", (error, data) => {
-    console.log("Error: ", error)
-    console.log('Data: ', data)
-})
+if(!address) {
+    console.log('Pleae provide an address')
+} else {
+    geocode(address, (error, data) => {
+        if (error){
+            return console.log(error)
+        }
+        
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+    
+            console.log(data.name)
+            console.log(forecastData)
+        })
+    })
+
+}
